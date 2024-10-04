78087fbb6a69a199795da9c85fb88e355f492b13
PhuocLoc <lephuocloc1729@gmail.com>
2024-10-04 15:57:48 +0700

    add README file

---
+++ README.md
@@ -0,0 +1,33 @@
+# Uber Data Pipeline
+
+## Prerequisites
+
+Before you can run this project, ensure you have the following installed on your machine:
+
+- [Docker](https://docs.docker.com/get-docker/)
+- [Docker Compose](https://docs.docker.com/compose/install/)
+
+## Installation
+
+### Clone the repository:
+
+```bash
+git clone https://github.com/lephuocloc1729/uber-data-pipeline
+cd uber-data-pipeline
+```
+
+## Running the Project
+
+To start the project using Docker Compose, run the following command:
+
+```bash
+docker compose up airflow-init
+```
+
+```bash
+docker compose up -d
+```
+
+## Accessing Airflow UI
+
+http://localhost:8080
--- dags/taxi_data_pipeline.py
+++ dags/taxi_data_pipeline.py
@@ -1,15 +1,12 @@
+from src import ingest_data, process_data, load_data
+from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from airflow import DAG
 import sys
 import os
 
 # Add src directory to Python path
 sys.path.append('/opt/airflow/src')
-
-
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
-from src import ingest_data, process_data, load_data
-
 
 
 def run_pipeline():
@@ -21,12 +18,12 @@ def run_pipeline():
 # Define the default arguments
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2024, 10, 3),
+    'start_date': datetime(2024, 10, 1),
     'retries': 1,
 }
 
 # Create the DAG
-with DAG('uber_taxi_dag', default_args=default_args, schedule_interval='@daily', catchup=False) as dag:
+with DAG('uber_taxi_dag1', default_args=default_args, schedule_interval='@daily', catchup=True) as dag:
 
     run_pipeline_task = PythonOperator(
         task_id='run_pipeline',
